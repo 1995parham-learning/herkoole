5664cbdb0e87fff185c346d52ce959c1fe48fab1
Parham Alvani <parham.alvani@gmail.com>
2020-05-16 02:42:40 +0430

    feat: Add Problem Model

---
+++ herkoole/model/model.py
@@ -0,0 +1,15 @@
+from chromosome import Chromosome
+
+import abc
+import typing
+
+
+class Model(abc.ABC):
+    @abc.abstractmethod
+    def initial_population(self, mu: int) -> typing.List[Chromosome]:
+        pass
+
+    @staticmethod
+    @abc.abstractstaticmethod
+    def chromosome_type():
+        pass
