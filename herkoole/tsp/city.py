ad1e851f848626bba1ff2b043d7dde75c61f26dc
Parham Alvani <parham.alvani@gmail.com>
2020-05-16 10:50:03 +0430

    feat: Complete TSP Model

--- herkoole/tsp/city.py
+++ herkoole/tsp/city.py
@@ -1,5 +1,8 @@
+import dataclasses
+
+
+@dataclasses.dataclass(repr=True)
 class City:
-    def __init__(self, identifier, x, y):
-        self.id = identifier
-        self.x = x
-        self.y = y
+    identifier: str
+    x: float
+    y: float
--- herkoole/tsp/model.py
+++ herkoole/tsp/model.py
@@ -37,10 +37,9 @@ class Chromosome(chromosome.Chromosome):
             raise ValueError("first create problem model")
 
     def __repr__(self):
-        res = ""
+        res = " -> ".join(repr(self.model.cities[gene]) for gene in self.genes)
 
-        for gene in self.genes:
-            res += f"{self.model.cities[gene]} -> "
+        res += f"\t fintess: {self.fitness():.4f}"
 
         return res
 
@@ -49,22 +48,51 @@ class Chromosome(chromosome.Chromosome):
         random.shuffle(self.genes)
 
     def fitness(self) -> float:
-        distance = 0
+        distance = 0.0
 
         for i in range(1, self.model.length):
             c1 = self.model.cities[self.genes[i - 1]]
             c2 = self.model.cities[self.genes[i]]
             distance += (c1.x - c2.x) ** 2 + (c1.y - c2.y) ** 2
 
-        return distance
+        return 1 / distance
 
     def mutate(self, prob: float):
         rand = random.random()
         if rand < prob:
-            random.shuffle(self.genes)
+            i = random.randrange(self.model.length)
+            j = random.randrange(self.model.length)
+            self.genes[i], self.genes[j] = self.genes[j], self.genes[i]
 
     @classmethod
     def crossover(
         cls, parent1: Chromosome, parent2: Chromosome, prob: float
     ) -> typing.Tuple[chromosome.Chromosome, chromosome.Chromosome]:
-        return parent1, parent2
+        rand = random.random()
+        if rand >= prob:
+            return parent1, parent2
+
+        cycles = [-1] * cls.model.length
+        cycle_no = 1
+        cyclestart = (i for i, v in enumerate(cycles) if v < 0)
+
+        for pos in cyclestart:
+            while cycles[pos] < 0:
+                cycles[pos] = cycle_no
+                pos = parent1.genes.index(parent2.genes[pos])
+
+        cycle_no += 1
+
+        child1 = Chromosome()
+        child2 = Chromosome()
+
+        child1.genes = [
+            parent1.genes[i] if n % 2 else parent2.genes[i]
+            for i, n in enumerate(cycles)
+        ]
+        child2.genes = [
+            parent2.genes[i] if n % 2 else parent1.genes[i]
+            for i, n in enumerate(cycles)
+        ]
+
+        return child1, child2
