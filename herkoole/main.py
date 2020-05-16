4d56ed6cda2da1eeacb4c9eeeb2fa344fa12d6b7
Parham Alvani <parham.alvani@gmail.com>
2020-05-16 11:58:53 +0430

    feat: Provide Functions for TSP/Knapsack

--- herkoole/main.py
+++ herkoole/main.py
@@ -1,10 +1,44 @@
 from ea import EvolutionaryAlgorithm
+from model import Model
 from knapsack import Model as KModel
 from tsp import Model as TModel, City
 
 import typing
 import logging
 import click
+import io
+
+
+def tsp(f: io.TextIOBase) -> Model:
+    cities: typing.List[City] = []
+    while True:
+        line = f.readline()
+        if line == "":
+            break
+        coordinate = line.split()
+        identifier = int(coordinate[0])
+        x = float(coordinate[1])
+        y = float(coordinate[2])
+        city = City(identifier, x, y)
+        cities.append(city)
+
+    return TModel(cities)
+
+
+def knapsack(f: io.TextIOBase) -> Model:
+    l1 = f.readline().split()
+    chromosome_length = int(l1[0])
+    max_weight = int(l1[1])
+
+    weights: typing.List[int] = []
+    values: typing.List[int] = []
+
+    for i in range(chromosome_length):
+        value_weight = f.readline().split()
+        values.append(int(value_weight[0]))
+        weights.append(int(value_weight[1]))
+
+    return KModel(weights, values, max_weight)
 
 
 @click.command()
@@ -21,37 +55,14 @@ def main(info, problem, iterations, verbose):
     if verbose is True:
         logging.basicConfig(level=logging.INFO)
 
-    if problem == "knapsack":
-        with open(info, "r") as f:
-            arr = f.readline().split()
-            chromosome_length = int(arr[0])
-            max_weight = int(arr[1])
-
-            weights: typing.List[int] = []
-            values: typing.List[int] = []
-
-            for i in range(chromosome_length):
-                value_weight = f.readline().split()
-                values.append(int(value_weight[0]))
-                weights.append(int(value_weight[1]))
-
-        m = KModel(weights, values, max_weight)
-    elif problem == "tsp":
-        cities = []
-        with open(info, "r") as f:
-            while True:
-                line = f.readline()
-                if line == "":
-                    break
-                coordinate = line.split()
-                identifier = int(coordinate[0])
-                x = float(coordinate[1])
-                y = float(coordinate[2])
-                city = City(identifier, x, y)
-                cities.append(city)
-        m = TModel(cities)
-    else:
-        return
+    m: Model
+    with open(info, "r") as f:
+        if problem == "knapsack":
+            m = knapsack(f)
+        elif problem == "tsp":
+            m = tsp(f)
+        else:
+            return
 
     EvolutionaryAlgorithm(10, 20, iterations, m).run()
 
