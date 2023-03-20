893a2be7740ca6334499e74b25f2481120ef9ed5
Parham Alvani <parham.alvani@gmail.com>
2023-03-20 21:47:28 +0330

    feat: update knapsack model

--- herkoole/knapsack/model.py
+++ herkoole/knapsack/model.py
@@ -1,3 +1,11 @@
+"""
+Knapsack problem is:
+
+Given a set of items, each with a weight and a value, determine
+which items to include in the collection so that the total weight
+is less than or equal to a given limit
+and the total value is as large as possible
+"""
 from __future__ import annotations
 import random
 
